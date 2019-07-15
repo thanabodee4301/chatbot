fe1561a78b02427976ae2f84d9ef53c1f7949ccb
unknown <bank@theiconweb.com>
2019-07-15 14:09:51 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict';
+const bodyParser = require('body-parser');
+const express = require('express');
+
+exports.test = (req,res)=> {
+    var returnMessage = "";
+    if(req.body.message == 'hi') {
+        returnMessage = 'Hello'
+    }
+    if(req.body.message == 'test') {
+      returnMessage == '555555'
+    }
+    if(req.body.message == 'ไง') {
+        returnMessage = 'ว่าไง'
+    } 
+    if(req.body.message == 'okokok') {
+        returnMessage = 'ok ไรวะ'
+    }
+    res.status(200).send(returnMessage);
+  }
